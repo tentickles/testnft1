17d3a6f3ccaf321d98a3e8bd57aed5e6fdfd3991
Max J Aguirre <tentickles@thedaodao.com>
2021-11-01 19:50:40 -0700

    first commit

---
+++ contracts/factoryNFT.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FactoryNFT is ERC721URIStorage { 
+
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Factory NFT", "FTN") {
+    }
+}
